8ae8f2f14c28e73b6b97fbb84ff3d2d1ae91e337
jenjd228 <ekocaba2@mail.ru>
2026-01-31 12:27:04 +0300

    init

---
+++ src/main/java/com/ai/simple_mcp/SimpleMcpApplication.java
@@ -0,0 +1,13 @@
+package com.ai.simple_mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMcpApplication.class, args);
+	}
+
+}
